a474bbb39ccedaa60859d9fabd81ce7b6934e6a3
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-02-02 21:05:58 +0100

    Edited bug in 4.js

--- 4.js
+++ 4.js
@@ -1,5 +1,6 @@
-const avg = (...arr) => arr.reduce((acc, item) => acc + item);
+const avg = (...arr) => arr.reduce((acc, item) => acc + item)/arr.length;
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-avg (grades);
+const scoree = avg (...grades);
+console.log(scoree);
 
 
